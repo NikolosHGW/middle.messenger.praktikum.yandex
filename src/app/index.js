f4ec78a851c37facbac4816990e19a25d6524be5
Bakarev <nic231731@yandex.ru>
2023-01-10 11:53:03 +0300

    refactor: add url spa

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,26 @@
 import indexFunction from './index.hbs';
 import './index.scss';
 
-document.body.innerHTML = indexFunction();
+const root = document.querySelector('#root');
+
+const loginPage = () => {
+  root.innerHTML = indexFunction();
+};
+
+const testPage = () => {
+  root.innerHTML = '<div>Hello Test Page!</div>';
+};
+
+const routes = {
+  '/': loginPage,
+  '/test': testPage,
+};
+
+const router = () => {
+  const route = routes[window.location.pathname];
+
+  route();
+};
+
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
