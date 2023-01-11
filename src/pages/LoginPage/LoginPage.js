8b90fb8eb7a487a3fbbeee0010a28e339588282d
Bakarev <nic231731@yandex.ru>
2023-01-11 11:09:48 +0300

    feat: create input

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,5 +1,5 @@
 import ui from './ui.hbs';
-import { Input } from '../../shared/Input/Input';
+import { Input } from '../../shared/ui/Input/Input';
 import './style.scss';
 
 const LoginPage = () => ui({ input: Input() });
