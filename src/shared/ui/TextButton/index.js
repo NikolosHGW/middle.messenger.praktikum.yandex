b4db93950a0e3b8ef9ec60eed118326d3e301c3d
Bakarev <nic231731@yandex.ru>
2023-01-13 20:43:45 +0300

    style: change margin for text button

--- src/shared/ui/TextButton/index.js
+++ src/shared/ui/TextButton/index.js
@@ -10,7 +10,7 @@ const TextButton = (
     isRed: false
   }
 ) => {
-  const className = isRed ? 'text-button text-button_color_red' : 'text-button'
+  const className = isRed ? 'text-button form__text-button text-button_color_red' : 'text-button form__text-button'
 
   return ui({ text, className });
 };
