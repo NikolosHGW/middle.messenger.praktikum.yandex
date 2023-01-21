50a54c49a4beb92c21dc64e40993fc0da679adfa
Bakarev <nic231731@yandex.ru>
2023-01-21 19:52:07 +0300

    feat: create event bus

---
+++ src/shared/lib/EventBus/EventBus.ts
@@ -0,0 +1,50 @@
+/* eslint-disable no-use-before-define */
+import { FuncVoid } from '../../utils/types/types';
+
+class EventBus {
+  private static instance: EventBus;
+
+  private listeners: Record<string, Array<FuncVoid>>;
+
+  constructor() {
+    this.listeners = {};
+  }
+
+  public static getInstance(): EventBus {
+    if (!EventBus.instance) {
+      EventBus.instance = new EventBus();
+    }
+
+    return EventBus.instance;
+  }
+
+  on(event: string, callback: FuncVoid) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(callback);
+  }
+
+  off(event: string, callback: FuncVoid) {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event] = this.listeners[event].filter(
+      (listener) => listener !== callback,
+    );
+  }
+
+  emit(event: string, ...args: []) {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event].forEach((listener) => {
+      listener(...args);
+    });
+  }
+}
+
+export { EventBus };
