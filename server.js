d77674d47423579e552787dd8723db92ff48279f
Bakarev <nic231731@yandex.ru>
2023-01-21 13:52:17 +0300

    refactor: solve review

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
