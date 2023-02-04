e0c840380b4dcf9685a4b9798c79d0ca67890676
Bakarev <nic231731@yandex.ru>
2023-02-04 16:14:59 +0300

    feat: validate message

--- src/entities/MessageFooter/model.ts
+++ src/entities/MessageFooter/model.ts
@@ -3,6 +3,17 @@ import { MessageInput } from '../../shared/ui/MessageInput';
 import { getInputTarget, logObjectToConsole } from '../../shared/utils/helpers';
 import { MessageFooterComponent } from './MessageFooterComponent';
 
+const validateMessage = (evt: Event) => {
+  const button = (evt.target as HTMLInputElement).closest('footer')?.querySelector('.send');
+  if (getInputTarget(evt.target).value === '') {
+    button?.setAttribute('disabled', 'true');
+    button?.classList.add('send_disabled');
+  } else {
+    button?.attributes.removeNamedItem('disabled');
+    button?.classList.remove('send_disabled');
+  }
+};
+
 const resultInput = {
   message: '',
 };
@@ -14,6 +25,8 @@ const MessageFooter = ({
       change: (evt: Event) => {
         resultInput.message = getInputTarget(evt.target).value;
       },
+      focus: validateMessage,
+      blur: validateMessage,
     },
   }),
   sendButton = Button({
